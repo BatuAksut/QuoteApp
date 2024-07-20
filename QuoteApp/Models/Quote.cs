bf5ed7a025e38698503df4f07490aef989d6f2ab
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-07-20 23:22:06 +0300

    Add project files.

---
+++ QuoteApp/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace QuoteApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteName { get; set; }
+
+        public string Author { get; set; }
+        public Quote()
+        {
+            
+        }
+    }
+}
